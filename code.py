950d5ecbbec2c5219d75028a70006f3f867cabdc
Liza Simendeeva <liza.simendeeva@mail.ru>
2022-07-19 12:54:44 +0700

    'new_start'

--- code.py
+++ code.py
@@ -1,2 +1,44 @@
-print("hello")
-print("li za")
+def calc(x, operand):
+    string = ""
+
+    for i in range(operand):
+        print("Введите операнд",
+              i + 1, end=": ")
+        a = int(input())
+
+        string += str(a)
+        string += " "
+
+        if i == operand - 1:
+            string += "="
+        else:
+            string += x
+            string += " "
+
+        if i == 0:
+            result = a
+        else:
+            if x == "*":
+                result *= a
+            elif x == ":":
+                result /= a
+            elif x == "+":
+                result += a
+            else:
+                result -= a
+
+    print(string, result)
+
+
+def check(x):
+    while True:
+        if (x != "*" and x != "+" and x != "-" and x != ":"):
+            x = input("Выберите корректную операцию: ")
+        else:
+            break
+
+
+x = input("Выберите операцию: ")
+check(x)
+operand = int(input("Сколько операндов? "))
+calc(x, operand)

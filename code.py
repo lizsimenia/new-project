eacd162c9af8a204fd40df0d9b95264a8618c019
Liza Simendeeva <liza.simendeeva@mail.ru>
2022-07-20 15:27:48 +0700

    'changed_for_PEP8'

--- code.py
+++ code.py
@@ -1,35 +1,43 @@
-def calc(x, operand):
+def calc(x, o):
     string = ""
 
-    for i in range(operand):
+    for i in range(o):
         print("Введите операнд", i + 1, end=": ")
         a = int(input())
+        res = 0
 
         string += str(a)
         string += " "
 
-        if i == operand - 1:
+        if i == o - 1:
             string += "="
         else:
             string += x
             string += " "
 
-        if i == 0: res = a
+        if i == 0:
+            res = a
 
         else:
-            if x == "*": res *= a
-            elif x == ":": res /= a
-            elif x == "+": res += a
-            else: res -= a
+            if x == "*":
+                res *= a
+            elif x == ":":
+                res /= a
+            elif x == "+":
+                res += a
+            else:
+                res -= a
 
     print(string, res)
 
+
 def check(x):
     while True:
-        if (x != "*" and x != "+" and x != "-" and x != ":"):
+        if x != "*" and x != "+" and x != "-" and x != ":":
             x = input("Введите корректную операцию (*, :, +, -): ")
         else:
             break
+
 
 operation = input("Введите операцию: ")
 check(operation)
